f77ddb255630ec84f9b392e3df0dde22b30790da
Armando Herrera <anhydrous99@gmail.com>
2020-07-16 00:54:54 -0500

    Added numba compilation for the physics functions, switched to the leap frog integration, and some parameter changes

--- OrbitDecayEnv.py
+++ OrbitDecayEnv.py
@@ -10,28 +10,34 @@ import gym
 
 
 @njit
-def _acceleration(r_input, v_input, thrust, angle, F_t, r_e, GM, m, C_d, A, steps, dt):
+def acc(r_input, v_input, thrust, angle, r_e, rm, GM, m, C_d, A, F_t):
+    r2 = np.dot(r_input, r_input)
+    r = np.sqrt(r2)
+    v2 = np.dot(v_input, v_input)
+    v = np.sqrt(v2)
+    t1 = F_t * thrust
+    t2 = np.array([t1 * np.cos(angle), t1 * np.sin(angle)])
+    r_unit = r_input / r
+    v_unit = v_input / v
+    h = r - r_e
+    rho = rm / ((7.8974E-24 + 8.89106E-31 * h) * (141.89 + 0.00299 * h) ** 11.388)
+    force = - (GM * m / r2) * r_unit
+    t = rho * v2 * C_d * A * v_unit
+    force -= rho * v2 * C_d * A * v_unit
+    force += t2
+    a = force / m
+    return a
+
+
+@njit
+def calculate_physics(x, v, thrust, angle, F_t, r_e, GM, m, C_d, A, steps, dt, rm):
     ndt = dt / steps
     for _ in range(steps):
-        r2 = np.dot(r_input, r_input)
-        r = np.sqrt(r2)
-        v2 = np.dot(v_input, v_input)
-        v = np.sqrt(v2)
-        t1 = F_t * thrust
-        t2 = np.array([t1 * np.cos(angle), t1 * np.sin(angle)])
-        r_unit = r_input / r
-        v_unit = v_input / v
-        h = r - r_e
-        rho = 1000 / ((7.8974E-24 + 8.89106E-31 * h) * (141.89 + 0.00299 * h) ** 11.388)
-        force = - (GM * m / r2) * r_unit
-        f = rho * v2 * C_d * A * v_unit
-        force -= f
-
-        force += t2
-        a = force / m
-        r_input += v_input * ndt + 0.5 * a * ndt ** 2
-        v_input += a * ndt
-    return r_input, v_input
+        a = acc(x, v, thrust, angle, r_e, rm, GM, m, C_d, A, F_t)
+        v = v + a * ndt / 2
+        x = x + v * ndt
+        v = v + a * ndt / 2
+    return x, v
 
 
 class OrbitDecayEnv(gym.Env):
@@ -41,7 +47,7 @@ class OrbitDecayEnv(gym.Env):
     }
 
     def __init__(self):
-        self.h = 5.5E5              # Height of satellite 550 km in meters
+        self.h = 5.95E5             # Height of satellite 550 km in meters
         self.r_e = 6.371E6          # Radius of earth in meters
         self.r_s = self.h + self.r_e
         self.m = 1000.0             # Mass of satellite
@@ -50,9 +56,9 @@ class OrbitDecayEnv(gym.Env):
         self.C_d = 2.123            # Drag coefficient
         self.A = 1.0                # Surface area normal to velocity
         self.F_t = 0.1              # Force of thrust
-        self.steps = 1000           # Step per dt
-        self.threshold = 12         # Threshold to end episode
-        self.rho_multiplier = 1000  # Rho is multiplied by this amount
+        self.steps = 5000           # Step per dt
+        self.threshold = 3          # Threshold to end episode
+        self.rho_multiplier = 10000 # Rho is multiplied by this amount
         self.orbit_v = None
         # Some state vectors
         self.r = np.zeros(2)
@@ -86,8 +92,8 @@ class OrbitDecayEnv(gym.Env):
         assert self.action_space.contains(action)
         thrust = action[0]
         angle = action[1] * 2 * np.pi
-        self.r, self.v = _acceleration(self.r, self.v, thrust, angle, self.F_t, self.r_e, self.GM, self.m, self.C_d,
-                                       self.A, self.steps, self.dt)
+        self.r, self.v = calculate_physics(self.r, self.v, thrust, angle, self.F_t, self.r_e, self.GM, self.m, self.C_d,
+                                           self.A, self.steps, self.dt, self.rho_multiplier)
         h = np.linalg.norm(self.r) - self.r_e
         hd = np.abs(self.h - h)  # Distance from intended height
         # Create output state and scale to between -1 and 1
@@ -98,6 +104,8 @@ class OrbitDecayEnv(gym.Env):
         if hd > self.threshold:
             reward = 0.0
             done = True
+        elif hd > self.threshold / 2:
+            reward = 0.5
 
         return state, reward, done, {}
 
@@ -129,7 +137,7 @@ def main():
 def main2():
     env = make_env()
     obs = env.reset()
-    for i in range(2500):
+    for i in range(3000):
         print(f'i: {i} obs {obs}')
         obs, rewards, dones, info = env.step([0.0, 0.0])
 
